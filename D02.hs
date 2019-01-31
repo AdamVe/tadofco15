2c1496505c38072c67e99b4bf4ac906ee9b274e9
Adam Velebil <adam.velebil@king.com>
2019-01-31 13:14:02 +0100

    D02 minimized

--- D02.hs
+++ D02.hs
@@ -1,16 +1,17 @@
 module Main where
 import qualified Data.ByteString.Lazy.Char8 as L
-import           Data.List
-import           Data.Maybe
+import Data.List
+import Data.Maybe
 
-p1 (w,h,l) = 3*w*h + 2*w*l + 2*h*l
-p2 (w,h,l) = w + w + h + h + w*h*l
+p1 (w,h,l)=3*w*h+2*w*l+2*h*l
+p2 (w,h,l)=2*w+2*h+w*h*l
 
-c s = (w,h,l)
-  where [w, h, l] = sort $ map (fst . fromJust . L.readInt) s
+c s=(w,h,l)
+  where [w,h,l]=sort$map(fst.fromJust.L.readInt)s
 
-run part = foldl1' (+) . map ( part . c . L.split 'x' ) . L.lines
+r p=foldl1'(+).map(p.c.L.split 'x').L.lines
 
-main = do
-        input <- L.getContents
-        print $ show (run p1 input) ++ "," ++ show (run p2 input)
+main=do
+  i<-L.getContents
+  print$r p1 i
+  print$r p2 i
