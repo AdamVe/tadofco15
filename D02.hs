eb947d75b8c244a1d4e03a18227b7ca24e84b09a
Adam Velebil <pardalek@gmail.com>
2019-01-31 07:48:59 +0100

    D02

--- D02.hs
+++ D02.hs
@@ -2,22 +2,30 @@
 
 module Main where
 import qualified Data.ByteString.Lazy.Char8 as L
-import Data.List
+import           Data.List
 
-part1 (w:h:l:[]) = 3*w*h + 2*w*l + 2*h*l
-part2 (w:h:l:[]) = w + w + h + h + w*h*l
+data Dimension = Dimension Int Int Int
+
+part1 (Dimension w h l) = 3*w*h + 2*w*l + 2*h*l
+part2 (Dimension w h l) = w + w + h + h + w*h*l
 
 readDim ∷ L.ByteString → Int
 readDim str =
   case L.readInt str of
-    Nothing → 0
+    Nothing         → 0
     Just (value, _) → value
 
-readDims ∷ [L.ByteString] → [Int]
-readDims (x:y:z:[]) = sort [readDim x, readDim y, readDim z]
+readDimension ∷ [L.ByteString] → Dimension
+readDimension [x,y,z] = Dimension w h l
+  where
+    w = sorted!!0
+    h = sorted!!1
+    l = sorted!!2
+    sorted = sort [readDim x, readDim y, readDim z]
+readDimension _ = error "Invalid"
 
-countResult part =Data.List.foldl1' (+) . map ( part . readDims . L.split 'x' ) . L.lines
-  
+countResult part = Data.List.foldl1' (+) . map ( part . readDimension . L.split 'x' ) . L.lines
+
 main :: IO ()
 main = do
         input ← L.getContents
