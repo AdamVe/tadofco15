93d4536d60df090007c4f1ef21f6b3655ad901e1
Adam Velebil <pardalek@gmail.com>
2019-03-03 09:59:22 +0100

    using lazy bytestrings

--- D06.hs
+++ D06.hs
@@ -1,31 +1,33 @@
 import           Data.Array
 import           Data.Bits
-import           Text.ParserCombinators.Parsec
+import qualified Data.ByteString.Lazy        as BL
+import           Text.Parsec
+import           Text.Parsec.ByteString.Lazy
 
 type Index = (Int, Int)
 type Grid = Array Index Bool
 type GridT = Grid -> Grid
 type Step = (Bool->Bool) -> (Index, Index) -> Grid -> Grid
 
-pInputP1 :: GenParser Char st [GridT]
+pInputP1 :: Parser [GridT]
 pInputP1 = (pLineP1 <* string "\n") `manyTill` eof
 
-pIndexP1 :: CharParser st Index
+pIndexP1 :: Parser Index
 pIndexP1 = many digit >>= (\i1 -> char ',' >> many digit >>= (\i2 -> return (read i1, read i2)))
 
-pBoundsP1 :: CharParser st (Index, Index)
+pBoundsP1 :: Parser (Index, Index)
 pBoundsP1 = pIndexP1 >>= (\i1 -> string " through " >> pIndexP1 >>= (\i2 -> return (i1,i2)))
 
-pTurnOnP1 :: CharParser st GridT
+pTurnOnP1 :: Parser GridT
 pTurnOnP1 = changeLamps (True .|.) <$> (string "turn on " >> pBoundsP1)
 
-pTurnOffP1 :: CharParser st GridT
+pTurnOffP1 :: Parser GridT
 pTurnOffP1 = changeLamps (False .&.) <$> (string "turn off " >> pBoundsP1)
 
-pToggleP1 :: CharParser st GridT
+pToggleP1 :: Parser GridT
 pToggleP1 = changeLamps not <$> (string "toggle " >> pBoundsP1)
 
-pLineP1 :: CharParser st GridT
+pLineP1 :: Parser GridT
 pLineP1 =  try pToggleP1 <|> try pTurnOffP1 <|> pTurnOnP1
 
 createGrid :: Int -> Int -> Grid
@@ -38,9 +40,9 @@ main :: IO ()
 main = do
   let g = createGrid 1000 1000
 
-  input <- getContents
+  input <- BL.getContents
   print "Starting!"
-  case parse pInputP1 "xx" input of
+  case parse pInputP1 "stdin" input of
     Left e -> print $ "Parser err" ++ show e
     Right list -> do
       let g' = foldl (\g'' f -> f g'') g list
