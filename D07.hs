7c70a163b12650d269a6edba28eadef4ad926820
Adam Velebil <pardalek@gmail.com>
2019-03-12 23:02:07 +0100

    using map

--- D07.hs
+++ D07.hs
@@ -2,34 +2,37 @@
 {-# OPTIONS_GHC -Wno-unused-imports #-}
 import qualified Data.ByteString.Lazy        as BL
 import           Data.List
+import           Data.Map
 import           Text.Parsec
 import           Text.Parsec.ByteString.Lazy
 
 type Gate = String
 data Signal = Value Int
-            | Reference String
+            | Reference Gate
 
-data Instruction
-  = And Gate Signal Signal
-  | Or Gate Signal Signal
-  | RShift Gate Signal Signal
-  | LShift Gate Signal Signal
-  | Not Gate Signal
-  | Var Gate Signal
+data Combi
+  = And Signal Signal
+  | Or Signal Signal
+  | RShift Signal Signal
+  | LShift Signal Signal
+  | Not Signal
+  | Var Signal
+
+type Instruction = Combi
 
 instance Show Signal where
   show (Value v)     = show v
   show (Reference r) = r
 
-instance Show Instruction where
-  show (And r v1 v2)    = show v1 ++ " AND " ++ show v2 ++ " -> " ++ r
-  show (Or r v1 v2)     = show v1 ++ " OR " ++ show v2 ++ " -> " ++ r
-  show (RShift r v1 v2) = show v1 ++ " RSHIFT " ++ show v2 ++ " -> " ++ r
-  show (LShift r v1 v2) = show v1 ++ " LSHIFT " ++ show v2 ++ " -> " ++ r
-  show (Not r v)        = "NOT " ++ show v ++ " -> " ++ r
-  show (Var r v)        = show v ++ " -> " ++ show r
+instance Show Combi where
+  show (And v1 v2)    = show v1 ++ " AND " ++ show v2
+  show (Or v1 v2)     = show v1 ++ " OR " ++ show v2
+  show (RShift v1 v2) = show v1 ++ " RSHIFT " ++ show v2
+  show (LShift v1 v2) = show v1 ++ " LSHIFT " ++ show v2
+  show (Not v)        = "NOT " ++ show v
+  show (Var v)        = show v
 
-pInput :: Parser [Instruction]
+pInput :: Parser [(Gate, Instruction)]
 pInput = (pExp <* char '\n') `manyTill` eof
 
 pVal :: Parser Int
@@ -51,19 +54,20 @@ pWspace = many $ oneOf " "
 pValue :: Parser Signal
 pValue = try (Value <$> pVal) <|> (Reference <$> pVar)
 
-pExp :: Parser Instruction
+pExp :: Parser (Gate, Instruction)
 pExp =
-  try (pValue >>= (\ v1 -> string "AND" >> pValue >>= (\ v2 -> pGate >>= (\ gate -> return (And gate v1 v2))))) <|>
-  try (pValue >>= (\ v1 -> string "OR" >> pValue >>= (\ v2 -> pGate >>= (\ gate -> return (Or gate v1 v2))))) <|>
-  try (pValue >>= (\ v1 -> string "RSHIFT" >> pValue >>= (\ v2 -> pGate >>= (\ gate -> return (RShift gate v1 v2))))) <|>
-  try (pValue >>= (\ v1 -> string "LSHIFT" >> pValue >>= (\ v2 -> pGate >>= (\ gate -> return (LShift gate v1 v2))))) <|>
-  try (string "NOT" >> pValue >>= (\ v1 -> pGate >>= (\ gate -> return (Not gate v1 )))) <|>
-  try (pValue >>= (\ v1 -> pGate >>= (\gate -> return $ Var gate v1)))
+  try (pValue >>= (\ v1 -> string "AND" >> pValue >>= (\ v2 -> pGate >>= (\ gate -> return (gate, And v1 v2))))) <|>
+  try (pValue >>= (\ v1 -> string "OR" >> pValue >>= (\ v2 -> pGate >>= (\ gate -> return (gate, Or v1 v2))))) <|>
+  try (pValue >>= (\ v1 -> string "RSHIFT" >> pValue >>= (\ v2 -> pGate >>= (\ gate -> return (gate, RShift v1 v2))))) <|>
+  try (pValue >>= (\ v1 -> string "LSHIFT" >> pValue >>= (\ v2 -> pGate >>= (\ gate -> return (gate, LShift v1 v2))))) <|>
+  try (string "NOT" >> pValue >>= (\ v1 -> pGate >>= (\ gate -> return (gate, Not v1)))) <|>
+  try (pValue >>= (\ v1 -> pGate >>= (\gate -> return (gate, Var v1))))
 
 main :: IO ()
 main = do
   input <- BL.getContents
   case parse pInput "stdin" input of
     Left e -> print $ "Parser err" ++ show e
-    Right list ->
+    Right list -> do
+      let mp = fromList list
       mapM_ print list
