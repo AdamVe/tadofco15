301d12f59ba06154f3d7d55346471c9912830ce0
Adam Velebil <pardalek@gmail.com>
2019-03-14 23:17:07 +0100

    D08 part 1

---
+++ D08.hs
@@ -0,0 +1,20 @@
+
+tupleAdd :: (Int, Int) -> (Int, Int) -> (Int, Int)
+tupleAdd (a,b) (c,d) = (a+c, b+d)
+
+count :: String -> (Int,Int)
+count []                = (0,0)
+count ('\\':'\\':xs)    = tupleAdd (2,1) (count xs)
+count ('\\':'\"':xs)    = tupleAdd (2,1) (count xs)
+count ('"':'"':xs)      = tupleAdd (2,0) (count xs)
+count ('\\':'x':_:_:xs) = tupleAdd (4,1) (count xs)
+count ('"':xs)          = tupleAdd (1,0) (count xs)
+count ('\n':xs)         = tupleAdd (0,0) (count xs)
+count (_:xs)            = tupleAdd (1,1) (count xs)
+
+main :: IO ()
+main = do
+  input <- getContents
+  let result = count input
+  print $ "counts: " ++ show result ++ " result: " ++ show (uncurry (-) result)
+
