87a82f2c6d196afb57ea4a4026fd4626640c96e7
Adam Velebil <adam.velebil@king.com>
2019-03-15 08:57:34 +0100

    D08 part 2 simplified

--- D08.hs
+++ D08.hs
@@ -1,17 +1,12 @@
+ta (a,b)(c,d) = (a + c, b + d)
 
-tupleAdd :: (Int, Int) -> (Int, Int) -> (Int, Int)
-tupleAdd (a,b) (c,d) = (a+c, b+d)
+cnt []        = (0,0)
+cnt ('\\':xs) = ta (2,1) (cnt xs)
+cnt ('"':xs)  = ta (2,1) (cnt xs)
+cnt ('\n':xs) = ta (2,0) (cnt xs)
+cnt (_:xs)    = ta (1,1) (cnt xs)
 
-count :: String -> (Int,Int)
-count []        = (0,0)
-count ('\\':xs) = tupleAdd (2,1) (count xs)
-count ('"':xs)  = tupleAdd (2,1) (count xs)
-count ('\n':xs) = tupleAdd (2,0) (count xs)
-count (_:xs)    = tupleAdd (1,1) (count xs)
-
-main :: IO ()
 main = do
-  input <- getContents
-  let result = count input
-  print $ "counts: " ++ show result ++ " result: " ++ show (uncurry (-) result)
+  i <- getContents
+  print (uncurry (-) (cnt i))
 
