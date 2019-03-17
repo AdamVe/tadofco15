95a9f0da2d259da66b0f978cc2ae0745c62c4d20
Adam Velebil <pardalek@gmail.com>
2019-03-17 10:59:50 +0100

    using lazy bytestring

--- D08.hs
+++ D08.hs
@@ -1,17 +1,22 @@
-
+import qualified Data.ByteString.Lazy as BL
 tupleAdd :: (Int, Int) -> (Int, Int) -> (Int, Int)
 tupleAdd (a,b) (c,d) = (a+c, b+d)
 
-count :: String -> (Int,Int)
-count []        = (0,0)
-count ('\\':xs) = tupleAdd (2,1) (count xs)
-count ('"':xs)  = tupleAdd (2,1) (count xs)
-count ('\n':xs) = tupleAdd (2,0) (count xs)
-count (_:xs)    = tupleAdd (1,1) (count xs)
+count :: BL.ByteString -> (Int,Int)
+count bs = if BL.length bs == 0
+              then (0,0)
+           else let tbs = BL.tail bs
+                    hbs = BL.head bs
+                    nextVal = count tbs in
+                  case hbs of
+                    0x5c -> tupleAdd (2,1) nextVal
+                    0x22 -> tupleAdd (2,1) nextVal
+                    0x0a -> tupleAdd (2,0) nextVal
+                    _    -> tupleAdd (1,1) nextVal
 
 main :: IO ()
 main = do
-  input <- getContents
-  let result = count input
-  print $ "counts: " ++ show result ++ " result: " ++ show (uncurry (-) result)
+  i <- BL.getContents
+  let r = count i
+  print $ "counts: " ++ show r ++ " result: " ++ show (fst r - snd r)
 
