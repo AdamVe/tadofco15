96b5afe78d8a2c31f42e109a4147e2eda0c7d159
Adam Velebil <pardalek@gmail.com>
2019-03-18 22:20:32 +0100

    D09 optimization?

--- D09.cc
+++ D09.cc
@@ -1,45 +1,27 @@
 #include <algorithm>
-#include <iostream>
 #include <limits>
+#include <map>
 #include <string>
-#include <unordered_map>
 #include <vector>
 
-struct index {
-  int from;
-  int to;
-};
-
-bool operator==(const index &fst, const index &snd) {
-  return fst.from == snd.from && fst.to == snd.to;
-}
-
-template <> struct std::hash<index> {
-  typedef index argument_type;
-  typedef std::size_t result_type;
-  result_type operator()(argument_type const &s) const noexcept {
-    result_type const h1(std::hash<int>{}(s.from));
-    result_type const h2(std::hash<int>{}(s.to));
-    return h1 ^ (h2 << 1);
-  }
-};
-
 int main() {
+  using dist_t = uint16_t;
   char from[21];
   char to[21];
-  int dist;
+  dist_t dist;
 
-  std::unordered_map<index, int> dists;
+  auto *dists = static_cast<dist_t *>(malloc(8 * 8 * sizeof(dist_t)));
   int loc_count = 0;
   while (true) {
     auto get_index = [&loc_count](const std::string &loc) {
-      static std::unordered_map<std::string, int> locs;
+      static std::map<std::string, int> locs;
       auto iter = locs.find(loc);
       if (iter == locs.end()) {
         locs[loc] = loc_count;
         loc_count = locs.size();
+        return loc_count - 1;
       }
-      return locs[loc];
+      return iter->second;
     };
 
     auto rv = scanf("%20s to %20s = %d\n", from, to, &dist);
@@ -50,12 +32,12 @@ int main() {
 
     auto ifrom = get_index(from);
     auto ito = get_index(to);
-    dists[{ifrom, ito}] = dist;
+    dists[(ifrom << 3) + ito] = dist;
   }
 
-  std::vector<int> v(loc_count);
+  std::vector<short> v(loc_count);
   std::generate(v.begin(), v.end(), []() {
-    static int i = 0;
+    static short i = 0;
     return i++;
   });
 
@@ -65,13 +47,8 @@ int main() {
     if (v.front() < v.back()) {
       int result = 0;
       for (int i = 0; i < loc_count - 1; i++) {
-        auto from = v[i];
-        auto to = v[i + 1];
-        if (from < to) {
-          result += dists[{from, to}];
-        } else {
-          result += dists[{to, from}];
-        }
+        result += dists[v[i] < v[i + 1] ? (v[i] << 3) + v[i + 1]
+                                        : (v[i + 1] << 3) + v[i]];
       }
       if (result < min_dist) {
         min_dist = result;
@@ -82,6 +59,6 @@ int main() {
     }
   } while (std::next_permutation(v.begin(), v.end()));
 
-  std::cout << "Min: " << min_dist << std::endl;
-  std::cout << "Max: " << max_dist << std::endl;
+  printf("%d %d\n", min_dist, max_dist);
+  free(dists);
 }
