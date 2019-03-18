9c3a4887d853244ed8b0be1506f952b90aff7e89
Adam Velebil <pardalek@gmail.com>
2019-03-18 21:04:11 +0100

    D09 with unordered map

--- D09.cc
+++ D09.cc
@@ -1,31 +1,78 @@
 #include <algorithm>
 #include <iostream>
 #include <limits>
+#include <string>
+#include <unordered_map>
 #include <vector>
 
-int main() {
-  std::vector<int> v = {0, 1, 2, 3, 4, 5, 6, 7};
+struct index {
+  int from;
+  int to;
+};
 
-  int dist[8][8] = {
-    {   0, 129,  58,  13,  24,  60,  71,  67},
-    { 129,   0, 142,  15, 135,  75,  82,  54},
-    {  58, 142,   0, 118, 122, 103,  49,  97},
-    {  13,  15, 118,   0, 116,  12,  18,  91},
-    {  24, 135, 122, 116,   0, 129,  53,  40},
-    {  60,  75, 103,  12, 129,   0,  15,  99},
-    {  71,  82,  49,  18,  53,  15,   0,  70},
-    {  67,  54,  97,  91,  40,  99,  70,   0}
-  };
+bool operator==(const index &fst, const index &snd) {
+  return fst.from == snd.from && fst.to == snd.to;
+}
+
+template <> struct std::hash<index> {
+  typedef index argument_type;
+  typedef std::size_t result_type;
+  result_type operator()(argument_type const &s) const noexcept {
+    result_type const h1(std::hash<int>{}(s.from));
+    result_type const h2(std::hash<int>{}(s.to));
+    return h1 ^ (h2 << 1);
+  }
+};
+
+int main() {
+  char from[21];
+  char to[21];
+  int dist;
+
+  std::unordered_map<index, int> dists;
+  int loc_count = 0;
+  while (true) {
+    auto get_index = [&loc_count](const std::string &loc) {
+      static std::unordered_map<std::string, int> locs;
+      auto iter = locs.find(loc);
+      if (iter == locs.end()) {
+        locs[loc] = loc_count;
+        loc_count = locs.size();
+      }
+      return locs[loc];
+    };
+
+    auto rv = scanf("%20s to %20s = %d\n", from, to, &dist);
+
+    if (rv != 3) {
+      break;
+    }
+
+    auto ifrom = get_index(from);
+    auto ito = get_index(to);
+    dists[{ifrom, ito}] = dist;
+  }
+
+  std::vector<int> v(loc_count);
+  std::generate(v.begin(), v.end(), []() {
+    static int i = 0;
+    return i++;
+  });
 
   int min_dist = std::numeric_limits<int>::max();
   int max_dist = 0;
   do {
     if (v.front() < v.back()) {
       int result = 0;
-      for (int i = 0; i < 7; i++) {
-        result += dist[v[i]][v[i + 1]];
+      for (int i = 0; i < loc_count - 1; i++) {
+        auto from = v[i];
+        auto to = v[i + 1];
+        if (from < to) {
+          result += dists[{from, to}];
+        } else {
+          result += dists[{to, from}];
+        }
       }
-
       if (result < min_dist) {
         min_dist = result;
       }
