3d127cfc660e95628678ca6779858e3e6c5ed35a
Adam Velebil <pardalek@gmail.com>
2019-03-18 18:42:42 +0100

    D08 shortened

--- D08.hs
+++ D08.hs
@@ -1,22 +1,21 @@
 import qualified Data.ByteString.Lazy as BL
-tupleAdd :: (Int, Int) -> (Int, Int) -> (Int, Int)
-tupleAdd (a,b) (c,d) = (a+c, b+d)
+ta :: (Int, Int) -> (Int, Int) -> (Int, Int)
+ta (a,b) (c,d) = (a+c, b+d)
 
-count :: BL.ByteString -> (Int,Int)
-count bs = if BL.length bs == 0
+cnt :: BL.ByteString -> (Int,Int)
+cnt bs = if BL.length bs == 0
               then (0,0)
            else let tbs = BL.tail bs
                     hbs = BL.head bs
-                    nextVal = count tbs in
+                    nextVal = cnt tbs in
                   case hbs of
-                    0x5c -> tupleAdd (2,1) nextVal
-                    0x22 -> tupleAdd (2,1) nextVal
-                    0x0a -> tupleAdd (2,0) nextVal
-                    _    -> tupleAdd (1,1) nextVal
+                    0x5c -> ta (2,1) nextVal
+                    0x22 -> ta (2,1) nextVal
+                    0x0a -> ta (2,0) nextVal
+                    _    -> ta (1,1) nextVal
 
 main :: IO ()
 main = do
   i <- BL.getContents
-  let r = count i
-  print $ "counts: " ++ show r ++ " result: " ++ show (fst r - snd r)
+  print (uncurry (-) (cnt i))
 
